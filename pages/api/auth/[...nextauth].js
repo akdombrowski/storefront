0afec64bb6c3456f83730ea3d225bc7c4262d229
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-01-08 10:00:41 -0600

    add custom pingone provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,26 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+export const PingOneProvider = () => {
+  return ({
+    id: "pingone",
+    name: "PingOne",
+    type: "oauth",
+    wellKnown: "https://auth.pingone.com/4e5491d5-74b6-4953-a3d1-c29f76f34d93/as/.well-known/openid-configuration",
+    authorization: { params: { scope: "openid email profile" } },
+    idToken: true,
+    checks: ["pkce", "state"],
+    profile(profile) {
+      return {
+        id: profile.sub,
+        name: profile.name,
+        email: profile.email,
+        image: profile.picture,
+      };
+    },
+  });
+}
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -9,6 +29,10 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
     // ...add more providers here
+    PingOneProvider({
+    clientId: process.env.development.local.PINGONE_ID,
+    clientSecret: process.env.development.local.PINGONE_SECRET,
+  })
   ],
 }
 
