5ccaf3c0e0d1a2e704218d8719dca5d11d287aa3
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-04-13 08:19:15 -0500

    update header button styles on landing page

--- styles/jss/nextjs-material-kit/pages/landingPage.ts
+++ styles/jss/nextjs-material-kit/pages/landingPage.ts
@@ -1,10 +1,11 @@
 import { container, title } from "../../nextjs-material-kit";
+import headerLinksStyle from "../components/headerLinksStyle";
 
-const landingPageStyle = {
+const landingPageStyle = (theme) => ({
   container: {
     zIndex: "12",
     color: "#aaa",
-    ...container
+    ...container,
   },
   title: {
     ...title,
@@ -13,24 +14,25 @@ const landingPageStyle = {
     marginTop: "30px",
     minHeight: "32px",
     color: "#FFFFFF",
-    textDecoration: "none"
+    textDecoration: "none",
   },
   subtitle: {
     fontSize: "1.313rem",
     maxWidth: "500px",
-    margin: "10px auto 0"
+    margin: "10px auto 0",
   },
   main: {
     background: "#000",
     position: "relative",
-    zIndex: "3"
+    zIndex: "3",
   },
   mainRaised: {
     margin: "-60px 30px 0px",
     borderRadius: "6px",
     boxShadow:
-      "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
-  }
-};
+      "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)",
+  },
+  ...headerLinksStyle(theme),
+});
 
 export default landingPageStyle;
