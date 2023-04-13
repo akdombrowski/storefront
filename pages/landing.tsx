34a19bcc02d17855d9440632343fc66c8430f55b
Anthony Dombrowski <akdflyinvee73@gmail.com>
2023-04-13 08:13:27 -0500

    updated text

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -78,7 +78,7 @@ export default function LandingPage(props) {
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={12} md={6}>
-              <h1 className={classes.title}>You're not a bot. Prove it.</h1>
+              <h1 className={classes.title}>You're not a bot. Prove it. Without the pain and suffering.</h1>
               <h3>Who doesn't love image challenge CAPTCHAs? Just kidding.</h3>
               <h4>
                 Everyone is pretty much sick of CAPTCHAs that ask you to choose
